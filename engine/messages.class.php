04e9f5eed26cb2c380f47d34086d854df7c73f9e
dsvan88 <dsvan88@gmail.com>
2021-10-28 12:28:38 +0300

    v0.1 Simplest way to handle requests from remote form

--- engine/messages.class.php
+++ engine/messages.class.php
@@ -3,6 +3,6 @@ require_once __DIR__.'/action.class.php';
 
 class Messages extends Action {
     public function getMessages(){
-        return $this->getAssocArray($this->query('SELECT * FROM '.TABLE_MAIN));
+        return $this->getAssocArray($this->query('SELECT * FROM '.TABLE_MAIN.' ORDER BY id DESC'));
     }
 }
