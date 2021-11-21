eb0266d902b369f9273686fb75a14d151360b28d
unknown <dsvan88@gmail.com>
2021-11-21 15:55:56 +0200

    Add simple Webhook for tg Bot

--- tech/tg-bot-webhook.php
+++ tech/tg-bot-webhook.php
@@ -15,7 +15,7 @@ file_put_contents($_SERVER['DOCUMENT_ROOT'].'/tg_bot-message.txt',print_r($_POST
 $array=[
     'name'=>"{$_POST['message']['from']['first_name']} {$_POST['message']['from']['last_name']} (@{$_POST['message']['from']['username']}, id:{$_POST['message']['from']['id']})",
     'email'=> '-',
-    'contact'=>$_POST['message']['chat']['first_name']."(id:{$_POST['chat']['from']['id']})",
+    'contact'=> ($_POST['message']['chat']['type'] === 'private') ? "{$_POST['message']['chat']['first_name']} {$_POST['message']['chat']['last_name']} (@{$_POST['message']['chat']['username']}, id:{$_POST['message']['chat']['id']})" : "{$_POST['message']['chat']['title']} (id:{$_POST['message']['chat']['id']})",
     'message'=> $_POST['message']['text'],
     'uid' => '1',
     'source' => 'TelegramBot',
@@ -27,3 +27,18 @@ require $_SERVER['DOCUMENT_ROOT'].'/engine/class.action.php';
 $action = new Action();
 
 $action->rowInsert($array);
+
+require $_SERVER['DOCUMENT_ROOT'].'/engine/class.bots.php';
+
+if (count($tg_uIDs) > 0){
+    $bot = new MessageBot();
+    $bot->message = "Yes, I hear you! Mr. $array[name]";    
+
+    try {
+        $messageSend = json_decode($bot->sendToTelegramBot($_POST['message']['chat']['id']), true);
+        $result['text'] .=  $messageSend['ok'] ? '<div>Message to telegram - send.</div>' : "<div>Message to telegram - not send ($messageSend[description])!</div>";
+    }
+    catch (Exception $e) {
+        $result['text'] .= "<div>Message not send!</div>";
+    }
+}
