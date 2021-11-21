3b6177d547a3e656c687da7de76eca8fe730f525
unknown <dsvan88@gmail.com>
2021-11-21 15:27:27 +0200

    Add simple Webhook for tg Bot

---
+++ tech/tg-bot-webhook.php
@@ -0,0 +1,26 @@
+<?
+if (strpos($contentType,'application/json') !==  false) {
+	$_POST = trim(file_get_contents('php://input'));
+	$_POST = json_decode($_POST, true);
+
+	if(!is_array($_POST)){
+		error_log(json_encode($_POST,JSON_UNESCAPED_UNICODE));
+        die('{"error":"1","title":"Error!","text":"Error: Nothing to send."}');
+    }
+}
+
+$array=[
+    'name'=>$_POST['message']['from']['first_name']."(id:${$_POST['message']['from']['id']})",
+    'email'=> '-',
+    'contact'=>$_POST['message']['chat']['first_name']."(id:${$_POST['chat']['from']['id']})",
+    'message'=> $_POST['message']['text'],
+    'uid' => '',
+    'source' => 'TelegramBot',
+    'time' => $_POST['message']['date'],
+];
+
+require $_SERVER['DOCUMENT_ROOT'].'/engine/class.action.php';
+
+$action = new Action();
+
+$action->rowInsert($array);
