ca64da049d02960572e88e3e3a271fab6987ddde
unknown <dsvan88@gmail.com>
2021-12-03 22:16:47 +0200

    Add simple Webhook for tg Bot

--- engine/class.bots.php
+++ engine/class.bots.php
@@ -35,7 +35,6 @@ class MessageBot{
 			// CURLOPT_SSL_VERIFYPEER => 0,
 		);
 		curl_setopt_array($curl , $options);
-       curl_exec($curl);
         if (is_array($userId) && isset($userId[1])){
             $newParams = $params;
             for($x=1; $x<count($userId); $x++){
